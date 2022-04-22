da08f2d280667f544755068709bc7ded5d1bdb5e
xrayd <72351211+xrayd@users.noreply.github.com>
2022-04-22 12:13:02 -0400

    created main and model shells

---
+++ main.py
@@ -0,0 +1,13 @@
+# implement train, test, and main all here!
+
+
+def main():
+    """
+    Reads data from chembl22/chembl22.h5, trains model, then tests model!
+    :return:
+    """
+    pass
+
+
+if __name__ == "__main__":
+    main()
